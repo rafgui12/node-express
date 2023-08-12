e92d7a75932bdc2833ca4ab53cb3cf3dc7096497
rsdual <rsdual12@gmail.com>
2023-08-12 12:57:58 -0500

    Workshop Week 1

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -21,4 +21,23 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites');
 });
 
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send the campsites ${req.params.campsiteId} to you`);
+})
+.post((req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.end(`Will update the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+.delete((req, res) => {
+    res.end('Deleting all campsites');
+});
+
 module.exports = campsiteRouter;
